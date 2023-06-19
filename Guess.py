f7c1e34fc30ab6c3e999577fabe08a43a9fad9be
Hybr1d758 <127874717+Hybr1d758@users.noreply.github.com>
2023-06-19 13:48:35 -0700

    even or odd fxn inserted

--- Guess.py
+++ Guess.py
@@ -1,8 +1,14 @@
 import random
-print('what is your name?')
+print("Hey!")
+print('What is your name?')
 name = input()
 secretNumber = random.randint(1, 20)
 print('Alright ' + name + ' Can you guess the number I am thinking about')
+
+def generate_three_number_hint():
+    hints = [secretNumber]
+    hints.append(7,17)
+    return hints
 
 for guessesTaken in range(1, 7):
     print('Take a guess')
@@ -10,9 +16,15 @@ for guessesTaken in range(1, 7):
     if guess < secretNumber:
         print('Your guess is too low')
     elif guess > secretNumber:
-        print(' Your guess is too high')
+        print('Your guess is too high')
     else:
         break
+
+if (guess % 2) == 0:
+    print("The answer is even")
+else:
+    print("The answer is odd")
+
 
 if guess == secretNumber:
     print('Well done ' + name)
